2348caf8cb98a0e1b6be797b1d628061acad81a2
Priyanka Umesh Pandit Tailapur <panditpriyanka93@gmail.com>
2019-04-15 10:31:07 -0600

    Add files via upload

---
+++ python2-features.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: priyanka
+"""
+
+
+print "Significant Features of Python2.x"
+print "List comprehensions provide a concise way to create lists"
+print "We can make new lists where each element is the result of some operations, or create a subsequence of elements that satisfy a certain condition."
+squares = [x**2 for x in range(8)]
+print(squares)
+
+#Result [0, 1, 4, 9, 16, 25, 36, 49]
